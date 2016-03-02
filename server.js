61325b635e42f91677f55533219fad66657f5240
root <root@mail.hacktivist.io>
2016-03-02 02:23:37 -0500

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var express = require('express');
 var rewrite = require('express-urlrewrite');
 var bodyParser = require('body-parser');
-var app = express();
+var app =  express();
 var Twitter = require('twitter');
 
 var twitterClient = new Twitter({
@@ -11,7 +11,7 @@ var twitterClient = new Twitter({
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   bearer_token: process.env.TWITTER_BEARER_TOKEN,
 });
-//app.set('port', (5000));
+// app.set('port', (5000));
 
 // route to get twitter info
 app.use('/resources', express.static(path.join(__dirname, 'public/resources')));
@@ -21,7 +21,9 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-exports.app = app;
-//app.listen(app.get('port'), function() {
-  //console.log('Server started: http://localhost:' + app.get('port') + '/');
-//});
+
+// app.listen(app.get('port'), function() {
+//   console.log('Server started: http://localhost:' + app.get('port') + '/');
+// });
+
+module.exports = app;
